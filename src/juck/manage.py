414e8d12cea7982cb85b9e9ff133906d43c13185
AlirezaFarahani <ar.d.farahani@gmail.com>
2014-01-15 12:17:30 +0330

    Fj bade un goh. merged with master

--- src/juck/manage.py
+++ src/juck/manage.py
@@ -5,11 +5,7 @@ import sys
 
 
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
-<<<<<<< HEAD
 sys.path.insert(1, '/home/alireza/Documents/Juck/lib')
-=======
-sys.path.insert(1, '/Users/Aref/Sites/juck/lib')
->>>>>>> 8a50be88bb48b0d1df8eb15874971fc522c4944b
 
 
 if __name__ == "__main__":
