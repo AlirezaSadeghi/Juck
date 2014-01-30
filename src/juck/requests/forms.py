efecfc7b84dbf3a57790eb310c6fb19f5ee4668d
Aref Shafaei <aref.shafaei@gmail.com>
2014-01-30 16:24:12 +0330

    scroll and shit

--- src/juck/requests/forms.py
+++ src/juck/requests/forms.py
@@ -9,8 +9,9 @@ class RequestForm(forms.Form):
     # receiver = forms.CharField(max_length=200, required=True, label=u'گیرنده', widget=forms.TextInput(attrs={'disabled': 'disabled'}))
 
     title = forms.CharField(max_length=250, required=True, label=u'عنوان')
-    cooperation_type = forms.ChoiceField(required=True, label=u'نوع همکاری',
+    cooperation_type = forms.ChoiceField(required=False, label=u'نوع همکاری',
                                          choices=(
+                                             (0, u'انتخاب کنید'),
                                              (1, u'تمام وقت'),
                                              (2, u'پاره وقت'),
                                              (3, u'دورکاری'),
