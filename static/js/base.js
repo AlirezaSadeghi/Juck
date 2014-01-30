6ce0d714abbb60ff34c4282d30cc9a7f6439fdad
Aref Shafaei <aref.shafaei@gmail.com>
2014-01-30 03:59:54 +0330

    ?!

--- static/js/base.js
+++ static/js/base.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
 
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+
+                return false;
+            }
+        }
+    });
+
+
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
 
-    $().toastmessage({sticky:true});
+    $().toastmessage({sticky: true});
     $('.ui.dropdown').dropdown();
 
     $('.index-column').hover(function () {
@@ -41,7 +56,7 @@ $(document).ready(function () {
 
     var onload = true;
     $(window).scroll(function () {
-        if(onload){
+        if (onload) {
             onload = false;
             return;
         }
@@ -49,7 +64,7 @@ $(document).ready(function () {
 
         var list = ['#hm', '#ji', '#jsi', '#ei'];
 
-        if(pos < 450){
+        if (pos < 450) {
             $("#hm").parent().addClass('active');
             list = ['#hm', '#ji', '#jsi', '#ei'];
             list.splice(0, 1);
@@ -64,26 +79,26 @@ $(document).ready(function () {
             list = ['#hm', '#ji', '#jsi', '#ei'];
             list.splice(2, 1);
         }
-        else if (pos >= 1990){
+        else if (pos >= 1990) {
             $("#ei").parent().addClass('active');
             list = ['#hm', '#ji', '#jsi', '#ei'];
             list.splice(3, 1);
         }
 
-        for(var item in list){
+        for (var item in list) {
             $(list[item]).parent().removeClass('active');
         }
     });
 
 });
 
-function doAjaxLogin(e){
-    if(e.keyCode == 13){
+function doAjaxLogin(e) {
+    if (e.keyCode == 13) {
         $("#login-btn").trigger('click');
     }
 }
 
-function message(msg, type ){
-    var stuff = 'show' + type  + 'Toast'
+function message(msg, type) {
+    var stuff = 'show' + type + 'Toast'
     $().toastmessage(stuff, msg);
 }
