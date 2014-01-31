0eff9afd6e14318cc727283c570ffd09380aa9a2
AlirezaFarahani <ar.d.farahani@gmail.com>
2014-01-31 19:06:34 +0330

    simple search

--- src/juck/question/filter.py
+++ src/juck/question/filter.py
@@ -3,12 +3,12 @@
 from django import forms
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.conf import settings
+from django.db.models.query_utils import Q
 from juck.question.models import Question
 
 
 class ManagerQuestionListFilter:
     class SecretaryQuestionListFilterForm(forms.Form):
-
 
         title = forms.CharField(label=u'عنوان', max_length=150, required=False, widget=forms.TextInput(
             attrs={'class': 'search-tab-content-input input-12', 'placeholder': u'عنوان سوال:'}))
@@ -16,6 +16,9 @@ class ManagerQuestionListFilter:
             attrs={'class': 'search-tab-content-input input-12', 'placeholder': u'محتوی پاسخ:'}))
         answered = forms.ChoiceField(label=u'وضعیت پاسخ', required=False, choices=(
             ('', u'وضعیت پاسخ'), (True, u'پاسخ داده شده'), (False, u'پاسخ داده نشده'), ))
+
+        both = forms.CharField(label=u'جستجو کلی', max_length=100, required=False, widget=forms.TextInput(
+            attrs={'placeholder': u'جستجو'}))
 
     Form = SecretaryQuestionListFilterForm
 
@@ -28,6 +31,7 @@ class ManagerQuestionListFilter:
             title = self.form.cleaned_data.get('title', '')
             answer = self.form.cleaned_data.get('answer', '')
             answered = self.form.cleaned_data.get('answered', '')
+            both = self.form.cleaned_data.get('both', '')
 
             if title:
                 filter_kwargs.update({'title__icontains': title})
@@ -37,6 +41,7 @@ class ManagerQuestionListFilter:
                 filter_kwargs.update({'answer__content__icontains': answer})
 
         questions = Question.objects.filter(**filter_kwargs)
+        questions = questions.filter((Q(title__icontains=both) | Q(answer__content__icontains=both)))
         count = questions.count()
 
         questions = questions.order_by('-timestamp')
